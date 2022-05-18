a0fd1aac6873d743daab1feb32c4077d691c97f8
kakon4366 <kakonbarman4366@gmail.com>
2022-05-18 23:02:10 +0600

    todo server initialization

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.co7pr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const todoCollection = client.db("usersBundle").collection("todos");
+
+		//get method
+	} finally {
+		// await client.close();
+	}
+}
+
+run().catch(console.dir());
+
+app.listen(port, () => {
+	console.log("This server is running port: " + port);
+});
